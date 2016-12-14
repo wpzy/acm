032784072eb38f4bb89e417460ad31cfde4486e4
wpzy <596320147@qq.com>
2016-12-14 13:38:21 +0800

    ok

--- BFS/main.c
+++ BFS/main.c
@@ -10,6 +10,8 @@ int *weight;
 int *value;
 int n;
 int calculate(int i,int j);
+int calculate2(int i,int j);
+int **dp;
 int main()
 {
 
@@ -17,7 +19,18 @@ scanf("%d",&n);
 weight=(int*)malloc(n*sizeof(int));
 value=(int*)malloc(n*sizeof(int));
 
+
+
+dp=malloc((n+1)*sizeof(int*));
+
 int i;
+for(i=0;i<=n;i++){
+    dp[i]=malloc((n+1)*sizeof(int));
+}
+//memset(dp,-1,sizeof(dp));
+int j;
+
+
 
 printf("input weight:\n");
 for(i=0;i<n;i++){
@@ -34,7 +47,13 @@ scanf("%d",&value[i]);
 int we;
 printf("input we:\n");
 scanf("%d",&we);
-int result=calculate(0,we);
+for(i=0;i<=n;i++){
+    for(j=0;j<=we;j++){
+    dp[i][j]=-1;
+    }
+}
+
+int result=calculate2(n,we);
 printf("result is :%d",result);
     return 0;
 }
@@ -42,6 +61,11 @@ printf("result is :%d",result);
 
 
 int calculate(int i,int j){
+if(dp[i][j]>=0){
+
+    return dp[i][j];
+}
+
 int sum;
 if(i==n){
 sum=0;
@@ -50,6 +74,26 @@ sum=max(calculate(i+1,j),calculate(i+1,j-weight[i])+value[i]);
 }else{
 sum=calculate(i+1,j);
 }
-return sum;
+dp[i][j]=sum;
+return dp[i][j];
 
 }
+
+int calculate2(int i,int j){
+printf("hello world\n");
+     if(dp[i][j]>=0){
+        return dp[i][j];
+     }
+
+     int sum;
+     if(i==0){
+        sum=0;
+     }else if(j<weight[i-1]){
+        sum=calculate2(i-1,j);
+        }else{
+            sum=max(calculate2(i-1,j),calculate2(i-1,j-weight[i-1])+value[i-1]);
+            }
+
+    return dp[i][j]=sum;
+}
+
